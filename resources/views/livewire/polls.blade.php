0c23badb0465842d8f670e46600c649436771e91
SalomeMumladze <salomemumladze775@gmail.com>
2023-11-17 18:25:08 +0400

    change style

--- resources/views/livewire/polls.blade.php
+++ resources/views/livewire/polls.blade.php
@@ -1,17 +1,18 @@
 <div>
     @forelse ($polls as $poll)
-      <div class="mb-4">
-        <h3 class="mb-4 text-xl">
+      <div class="mb-4 bg-slate-100 rounded-md p-2">
+        <h3 class="mb-1 text-xl">
           {{ $poll->title }}
         </h3>
         @foreach ($poll->options as $option)
-        <div class="mb-2">
-          <button class="btn"
-            wire:click="vote({{ $option->id }})">Vote</button>
-          {{ $option->name }} ({{ $option->votes->count() }})
-        </div>
-      @endforeach
+          <div class="mb-4">
+            <button class="btn"
+              wire:click="vote({{ $option->id }})">Vote</button>
+            {{ $option->name }} ({{ $option->votes->count() }})
+          </div>
+        @endforeach
       </div>
+      <hr class="mb-4"/>
     @empty
       <div class="text-gray-500">
         No polls available
